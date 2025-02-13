6499307229eb54fbcba89c5203ea53d8385fee3f
jossyboydgenius <jossyboydgenius@gmail.com>
2025-02-13 06:51:49 +0000

    Add AppRoutes class to define application navigation routes

---
+++ lib/app/routes/app_routes.dart
@@ -0,0 +1,7 @@
+class AppRoutes {
+  static const String splash = '/';
+  static const String onboarding = '/onboarding';
+  static const String authentication = '/authentication';
+  static const String dashboard = '/dashboard';
+  static const String signUp = '/sign-up';
+} 
