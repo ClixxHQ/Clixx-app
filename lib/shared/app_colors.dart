c09898acf2a7550294e6c2a90d27d250eed61b5c
jossyboydgenius <jossyboydgenius@gmail.com>
2025-02-13 08:30:24 +0000

    Add AppColors class for centralized color management

---
+++ lib/shared/app_colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFF208BFE);
+  static const Color primitiveBlue = Color(0xFFE9F3FF);
+  static const Color grey100 = Color(0xFFF3F4F6);
+  static const Color grey200 = Color(0xFFE5E7EB);
+  static const Color grey300 = Color(0xFF9CA3AF);
+  static const Color grey400 = Color(0xFF6B7280);
+  static const Color grey800 = Color(0xFF1F2937);
+} 
