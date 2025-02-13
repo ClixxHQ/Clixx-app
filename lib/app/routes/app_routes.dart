7c5b3afac101599d1128d5b0f7de2bd5f3da515d
jossyboydgenius <jossyboydgenius@gmail.com>
2025-02-13 08:30:08 +0000

    Refactor app routes to include onboarding and sign-up views, and set initial route

--- lib/app/routes/app_routes.dart
+++ lib/app/routes/app_routes.dart
@@ -1,7 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:clixx/ui/views/onboarding/splash_screen_view.dart';
+import 'package:clixx/ui/views/onboarding/onboarding_view.dart';
+import 'package:clixx/ui/views/authentication/sign_up_view.dart';
+
 class AppRoutes {
+  // Base Routes
   static const String splash = '/';
   static const String onboarding = '/onboarding';
-  static const String authentication = '/authentication';
-  static const String dashboard = '/dashboard';
   static const String signUp = '/sign-up';
+
+  static const String initialRoute = splash;
+
+  static Map<String, WidgetBuilder> routes = {
+    splash: (context) => const SplashScreenView(),
+    onboarding: (context) => const OnboardingView(),
+    signUp: (context) => const SignUpView(),
+  };
 } 
