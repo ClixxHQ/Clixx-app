cd323f3459db86bff0b3c7ada0e98a80b548be67
jossyboydgenius <jossyboydgenius@gmail.com>
2025-02-13 08:30:28 +0000

    Add AppSpacing class for centralized spacing management

---
+++ lib/shared/app_spacing.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class AppSpacing {
+  static SizedBox h4() => SizedBox(width: 4.w);
+  static SizedBox h8() => SizedBox(width: 8.w);
+  static SizedBox h12() => SizedBox(width: 12.w);
+  static SizedBox h16() => SizedBox(width: 16.w);
+  static SizedBox h24() => SizedBox(width: 24.w);
+  static SizedBox h32() => SizedBox(width: 32.w);
+
+  static SizedBox v4() => SizedBox(height: 4.h);
+  static SizedBox v8() => SizedBox(height: 8.h);
+  static SizedBox v12() => SizedBox(height: 12.h);
+  static SizedBox v16() => SizedBox(height: 16.h);
+  static SizedBox v24() => SizedBox(height: 24.h);
+  static SizedBox v32() => SizedBox(height: 32.h);
+  static SizedBox v64() => SizedBox(height: 64.h);
+} 
