350d5a6d6c01c4bccc138838d7166736c154b578
jossyboydgenius <jossyboydgenius@gmail.com>
2025-02-13 06:51:40 +0000

    Add AppTheme class with light and dark theme configurations

---
+++ lib/app/themes.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static ThemeData lightTheme = ThemeData(
+    useMaterial3: true,
+    colorScheme: ColorScheme.fromSeed(
+      seedColor: Colors.blue,
+      brightness: Brightness.light,
+    ),
+    scaffoldBackgroundColor: Colors.white,
+  );
+
+  static ThemeData darkTheme = ThemeData(
+    useMaterial3: true,
+    colorScheme: ColorScheme.fromSeed(
+      seedColor: Colors.blue,
+      brightness: Brightness.dark,
+    ),
+  );
+} 
