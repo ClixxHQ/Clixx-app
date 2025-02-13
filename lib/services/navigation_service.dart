5ff6a44a7a6d8173886c04fdc155a2ec9a51ba19
jossyboydgenius <jossyboydgenius@gmail.com>
2025-02-13 09:48:19 +0000

    Enhance NavigationService with additional route management methods and current route retrieval

--- lib/services/navigation_service.dart
+++ lib/services/navigation_service.dart
@@ -1,5 +1,16 @@
 import 'package:flutter/material.dart';
 
+extension NavigatorStateX on NavigatorState {
+  String? get currentRouteName {
+    String? name;
+    popUntil((route) {
+      name = route.settings.name;
+      return true;
+    });
+    return name;
+  }
+}
+
 class NavigationService {
   static GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 
@@ -7,27 +18,54 @@ class NavigationService {
 
   static BuildContext get context => navigatorKey.currentContext!;
 
-  static Future<dynamic> pushNamed(String routeName, {Object? arguments}) {
-    return navigator.pushNamed(routeName, arguments: arguments);
+  static Future<dynamic> removeUntill(String routeName) async {
+    return navigatorKey.currentState
+        ?.popUntil((route) => route.settings.name == routeName);
   }
 
-  static Future<dynamic> pushReplacementNamed(String routeName, {Object? arguments}) {
-    return navigator.pushReplacementNamed(routeName, arguments: arguments);
+  static Future<dynamic> pushNamed(String routeName,
+      {dynamic arguments}) async {
+    return navigatorKey.currentState
+        ?.pushNamed(routeName, arguments: arguments);
   }
 
-  static Future<dynamic> pushNamedAndRemoveUntil(String routeName, {Object? arguments}) {
-    return navigator.pushNamedAndRemoveUntil(
+  static Future<dynamic> pushReplacementNamed(String routeName,
+      {dynamic arguments}) async {
+    return navigatorKey.currentState
+        ?.pushReplacementNamed(routeName, arguments: arguments);
+  }
+
+  static bool canPop() {
+    return navigatorKey.currentState?.canPop() ?? false;
+  }
+
+  static String? currentRoute() {
+    return NavigationService.navigatorKey.currentState?.currentRouteName;
+  }
+
+  static void goBack() {
+    return navigatorKey.currentState?.pop();
+  }
+
+  static Future<dynamic> pushNamedAndRemoveUntil(String routeName,
+      {bool routePredicate = false, dynamic arguments}) async {
+    return navigatorKey.currentState?.pushNamedAndRemoveUntil(
+        routeName, (route) => routePredicate,
+        arguments: arguments);
+  }
+
+  static Future<dynamic> closeAllAndPushNamed(String routeName,
+      {bool routePredicate = false, dynamic arguments}) async {
+    return navigatorKey.currentState?.pushNamedAndRemoveUntil(
       routeName,
       (route) => false,
       arguments: arguments,
     );
   }
 
-  static void pop([dynamic result]) {
-    return navigator.pop(result);
-  }
-
-  static void popUntil(String routeName) {
-    navigator.popUntil(ModalRoute.withName(routeName));
+  static Future<dynamic> popAndPushNamed(String routeName,
+      {dynamic arguments}) async {
+    return navigatorKey.currentState
+        ?.popAndPushNamed(routeName, arguments: arguments);
   }
 } 
