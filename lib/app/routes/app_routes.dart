21da54a057d225152ea2a02a0dc3c2b2939a36e1
jossyboydgenius <jossyboydgenius@gmail.com>
2025-02-13 11:23:31 +0000

    Add OTP verification route and view with argument handling

--- lib/app/routes/app_routes.dart
+++ lib/app/routes/app_routes.dart
@@ -2,12 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:clixx/ui/views/onboarding/splash_screen_view.dart';
 import 'package:clixx/ui/views/onboarding/onboarding_view.dart';
 import 'package:clixx/ui/views/authentication/sign_up_view.dart';
+import 'package:clixx/ui/views/authentication/otp_verification_view.dart';
 
 class AppRoutes {
   // Base Routes
   static const String splash = '/';
   static const String onboarding = '/onboarding';
   static const String signUp = '/sign-up';
+  static const String otpVerification = '/otp-verification';
 
   static const String initialRoute = splash;
 
@@ -15,5 +17,16 @@ class AppRoutes {
     splash: (context) => const SplashScreenView(),
     onboarding: (context) => const OnboardingView(),
     signUp: (context) => const SignUpView(),
+    otpVerification: (context) {
+      final args = ModalRoute.of(context)!.settings.arguments as Map<String, dynamic>;
+      return OtpVerificationView(
+        phoneNumber: args['phoneNumber'],
+        email: args['email'],
+        firstName: args['firstName'],
+        lastName: args['lastName'],
+        birthday: args['birthday'],
+        referralCode: args['referralCode'],
+      );
+    },
   };
 } 
