6fca2209f03d56e0527330986f99db454383e243
jossyboydgenius <jossyboydgenius@gmail.com>
2025-02-14 02:04:10 +0000

    Enhance OTP input field behavior with backspace handling and focus management

--- lib/shared/widgets/app_otp_input.dart
+++ lib/shared/widgets/app_otp_input.dart
@@ -26,7 +26,7 @@ class AppOtpInput extends StatelessWidget {
             (index) => Container(
               width: 24.w,
               margin: EdgeInsets.symmetric(horizontal: 4.w),
-              child: _buildTextField(index),
+              child: _buildTextField(index, context),
             ),
           ),
         ),
@@ -37,7 +37,7 @@ class AppOtpInput extends StatelessWidget {
             (index) => Container(
               width: 24.w,
               margin: EdgeInsets.symmetric(horizontal: 4.w),
-              child: _buildTextField(index + 3),
+              child: _buildTextField(index + 3, context),
             ),
           ),
         ),
@@ -45,37 +45,61 @@ class AppOtpInput extends StatelessWidget {
     );
   }
 
-  Widget _buildTextField(int index) {
-    return TextFormField(
-      controller: controllers[index],
-      focusNode: focusNodes[index],
-      keyboardType: TextInputType.number,
-      textAlign: TextAlign.center,
-      style: TextStyle(
-        fontSize: 24.sp,
-        fontWeight: FontWeight.w600,
-        color: Colors.black,
-      ),
-      decoration: InputDecoration(
-        contentPadding: EdgeInsets.zero,
-        enabledBorder: UnderlineInputBorder(
-          borderSide: BorderSide(
-            color: Colors.black,
-            width: 4.w,
+  Widget _buildTextField(int index, BuildContext context) {
+    return RawKeyboardListener(
+      focusNode: FocusNode(),
+      onKey: (RawKeyEvent event) {
+        if (event is RawKeyDownEvent && 
+            event.logicalKey == LogicalKeyboardKey.backspace && 
+            controllers[index].text.isEmpty && 
+            index > 0) {
+          controllers[index - 1].clear();
+          focusNodes[index - 1].requestFocus();
+          return;
+        }
+      },
+      child: TextFormField(
+        controller: controllers[index],
+        focusNode: focusNodes[index],
+        keyboardType: TextInputType.number,
+        textAlign: TextAlign.center,
+        style: TextStyle(
+          fontSize: 24.sp,
+          fontWeight: FontWeight.w600,
+          color: Colors.black,
+        ),
+        decoration: InputDecoration(
+          contentPadding: EdgeInsets.zero,
+          enabledBorder: UnderlineInputBorder(
+            borderSide: BorderSide(
+              color: Colors.black,
+              width: 4.w,
+            ),
+          ),
+          focusedBorder: UnderlineInputBorder(
+            borderSide: BorderSide(
+              color: AppColors.primary,
+              width: 4.w,
+            ),
           ),
         ),
-        focusedBorder: UnderlineInputBorder(
-          borderSide: BorderSide(
-            color: AppColors.primary,
-            width: 4.w,
-          ),
-        ),
+        inputFormatters: [
+          LengthLimitingTextInputFormatter(1),
+          FilteringTextInputFormatter.digitsOnly,
+        ],
+        onChanged: (value) {
+          if (value.isNotEmpty) {
+            onChanged(value, index);
+            if (index < 5) {
+              focusNodes[index + 1].requestFocus();
+            }
+          } else if (index > 0) {
+            // When text is deleted, move to previous field
+            focusNodes[index - 1].requestFocus();
+          }
+        },
+        onTapOutside: (event) => FocusScope.of(context).unfocus(),
       ),
-      inputFormatters: [
-        LengthLimitingTextInputFormatter(1),
-        FilteringTextInputFormatter.digitsOnly,
-      ],
-      onChanged: (value) => onChanged(value, index),
     );
   }
 } 
