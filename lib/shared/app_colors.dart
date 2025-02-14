394c3e390e139de371959802c39d89bfa569c310
jossyboydgenius <jossyboydgenius@gmail.com>
2025-02-14 10:26:31 +0000

    Add new color constants for improved theming and consistency

--- lib/shared/app_colors.dart
+++ lib/shared/app_colors.dart
@@ -4,10 +4,15 @@ class AppColors {
   static const Color primary = Color(0xFF208BFE);
   static const Color primitiveBlue50 = Color(0xFFA6D1FF);
   static const Color primitiveBlue400 = Color(0xFF4DA2FE);
+  static const Color primitiveBlue900 = Color(0xFF0A2A4C);
+  static const Color primitiveGrey600 = Color(0xFF525252);
+  static const Color primitiveGrey700 = Color(0xFF404040);
+  static const Color background = Color(0xFFF5F5F5);
   static const Color grey100 = Color(0xFFF3F4F6);
   static const Color grey200 = Color(0xFFE5E7EB);
   static const Color grey300 = Color(0xFF9CA3AF);
   static const Color grey400 = Color(0xFF6B7280);
   static const Color grey500 = Color(0xFF737373);
+  static const Color grey600 = Color(0xFFE5E5E5);
   static const Color grey800 = Color(0xFF1F2937);
 } 
